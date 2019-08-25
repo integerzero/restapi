84830b865bdd23cbf4efbfd951d83d301de0974d
integerzero <steadyprogrammer@gmail.com>
2019-08-25 16:27:39 +0900

    restapi first commit

---
+++ integerzero-restapi/src/main/java/jp/integerzero/restapi/integerzerorestapi/IntegerzeroRestapiApplication.java
@@ -0,0 +1,13 @@
+package jp.integerzero.restapi.integerzerorestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntegerzeroRestapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntegerzeroRestapiApplication.class, args);
+	}
+
+}
---
+++ integerzero-restapi/src/test/java/jp/integerzero/restapi/integerzerorestapi/IntegerzeroRestapiApplicationTests.java
@@ -0,0 +1,16 @@
+package jp.integerzero.restapi.integerzerorestapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IntegerzeroRestapiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
